df9dcb962b2993872acabf886a99d37589102815
br3y1n <brayan@plasticolab.com>
2020-01-27 10:41:13 -0500

    17

--- src/App.js
+++ src/App.js
@@ -1,18 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './assets/images/logo.svg';
 import './assets/css/App.css';
 
-//imports
-import MyComponent from './components/MyComponent'
+/*//imports
+//import MyComponent from './components/MyComponent'
+
+//function Test(attr) { // form 1
+  //return <h2>{attr.title}</h2>
+//}
+
+//const Test = (at) => <h2>{at.title}</h2> // form 2
+
+class Test extends Component { // form 3
+
+  render() {
+    return <h2>{this.props.title}</h2> //props is a reserved word
+  }
+}
+
+class Text extends Component {
+  render() {
+    const {
+      text,
+      number,
+      array,
+      callback
+    } = this.props //destructuring 
+
+    return <div>
+      <p>{text}</p>
+      <p>{number}</p>
+      <p>{array.map(callback).join(', ')}</p>
+      <p>{this.props.propByDf}</p>
+    </div>
+  }
+}
+
+Text.defaultProps = {
+  propByDf: 'esta es por defecto'
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload el brayan.
-        </p>
+        <Test title='Probando' />
+        <Text
+          text="que buen texto"
+          number={1}
+          array={[1, 2, 3]}
+          callback={(val) => val * 2} />
 
         <section className="components">
           <MyComponent />
@@ -22,4 +60,9 @@ function App() {
   );
 }
 
-export default App;
+export default App;*/
+
+
+class App extends Component {
+
+}
